68c321aaf85892ddeb853fe7b8260ceab20ad2e6
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2021-03-25 15:01:30 -0500

    Add restaurant item react

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  # get 'home/index'
+  root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json} do
     namespace :v1 do
       resources :restaurants, only: %i[show index]
-      resources :comments, only: %i[create]
+      resources :pictures, only: %i[index]
+      resources :comments, only: %i[create index]
     end
   end
 end
