b71255bb51b9573e5de2aed75d7209fa6fb67c21
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2021-03-25 15:01:22 -0500

    Add restaurant item react

---
+++ app/javascript/packs/services/restaurant.service.jsx
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+export const RESTAURANT_ENDPOINT = 'http://localhost:5000/api/v1/restaurants';
+
+export function getRestaurantsAsync() {
+  return axios.get(RESTAURANT_ENDPOINT);
+}
