a229140a97b8b350805a8122a33a82f3869287ff
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2021-03-25 09:02:17 -0500

    add heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'
+  root to: 'home#index'
+  # mount Rswag::Ui::Engine => '/api-docs'
+  # mount Rswag::Api::Engine => '/api-docs'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json} do
     namespace :v1 do
